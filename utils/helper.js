c74a85617914b951cb93445b0ec5902fdd405f7c
hoabc <hoabc@basebs.com>
2023-12-09 14:33:47 +0700

    Commit ++

--- utils/helper.js
+++ utils/helper.js
@@ -56,7 +56,7 @@ const timeConvert = (time) => {
 
 let sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
-const refetchGetVol = async (coupleFilters, timeToSleep = 1500) => {
+const refetchGetVol = async (coupleFilters, timeToSleep = 3000) => {
     let isComplete = false;
     let sellVol = coupleFilters.sellVol;
     let buyVol = coupleFilters.buyVol;
