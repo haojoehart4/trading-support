311533d5e9ae0671f31384c0aef1673b5c590e69
haojoehart4 <hoabc@basebs.com>
2024-04-06 23:45:32 +0700

    CM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const crypto = require('crypto')
+const crypto = require("crypto");
 const cors = require("cors");
 const Binance = require("node-binance-api");
 const cookieParser = require("cookie-parser");
@@ -210,38 +210,59 @@ const closeInterval = () => {
 
 bot.on("message", (msg) => {
   if (msg.text.toString().toLowerCase().indexOf("balance") !== -1) {
-    // binance.balance((error, balances) => {
-    //   if (error) return console.error(error);
-    //   let balanceResult = [];
-    //   for (const x in balances) {
-    //     if (parseFloat(balances[x].available) > 0) {
-    //       balanceResult.push(`${x}: ${balances[x].available}`);
-    //     }
-    //   }
-    //   const responseToUser = balanceResult.join(", ");
-    //   bot.sendMessage(
-    //     msg.chat.id,
-    //     `Your balance information here: ${responseToUser}`
-    //   );
-    // });
-    const timestamp = Date.now();
-    const signature = crypto
-      .createHmac("sha256", process.env.APISECRET)
-      .update(`timestamp=${timestamp}`)
-      .digest("hex");
-    const params = {
-      timestamp,
-      signature,
-      recvWindow: 5000, // Optional, milliseconds
-    };
-    axios
-      .get("https://api.binance.com/api/vs/account", { params })
-      .then((response) => {
-        console.log('haha', response.data); // Your account information
-      })
-      .catch((error) => {
-        console.error(error);
+    try{
+      binance.balance((error, balances) => {
+        if (error) return console.error(error);
+        let balanceResult = [];
+        for (const x in balances) {
+          if (parseFloat(balances[x].available) > 0) {
+            balanceResult.push(`${x}: ${balances[x].available}`);
+          }
+        }
+        const responseToUser = balanceResult.join(", ");
+        bot.sendMessage(
+          msg.chat.id,
+          `Your balance in  formation here: ${responseToUser}`
+        );
       });
+    } catch(err) {
+      console.log(err)
+    }
+
+
+    // axios
+    //   .get("https://api.binance.com/api/v3/time")
+    //   .then((resTime) => {
+    //     const timestamp = resTime.data.serverTime
+    //     const signature = crypto
+    //     .createHmac(
+    //       "sha256",
+    //       "bNEFxV63lDtM8mms1yNW2hdL84UYHQ4jjKlhFuWtjm4q1yg2NCP11Un9KhDDvWLk"
+    //     )
+    //     .update(`timestamp=${timestamp}`)
+    //     .digest("hex");
+
+    //     const params = {
+    //       timestamp,
+    //       signature,
+    //       recvWindow: 60000, // Optional, milliseconds
+    //     };
+
+    //     const headers = {
+    //       'X-MBX-APIKEY': 'VzhnAiTAbBE8hZYtVYrChohwAVWbB2XMqXbsUdZ2CmxdAxz44GkfNbF07DfRPn3i',
+    //   }
+    //     axios
+    //     .get("https://api.binance.com/api/v3/account", { params, headers })
+    //     .then((response) => {
+    //       console.log('haha', response.data); // Your account information
+    //     })
+    //     .catch((error) => {
+    //       console.error(error);
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   }
 
   if (msg.text.toString().toLowerCase().indexOf("invest new token") !== -1) {
